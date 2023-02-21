c789c60f1456543dfe94c11fba6957c990139ad1
Bernar001 <adubernard449@gmail.com>
2023-02-21 13:10:57 -0800

    file0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints the string "_putchar" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+char y[] = {'_', 'p', 'u', 't', 'c', 'h',
+	    'a', 'r'};
+int h = 0;
+while (y[h] != '\0')
+{
+_putchar(y[h]);
+
+h++;
+}
+_putchar('\n');
+return (0);
+}
